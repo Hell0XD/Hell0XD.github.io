35dcbfe20183b75a0e7bc0472edfd5c2313bc856
Hell0XD <>
2023-07-25 20:16:55 +0200

    optimized images, added cv and updated base site

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://max.muller.live",
+  site: "https://maxmuller.me",
   integrations: [tailwind(), sitemap(), compress(), mdx(), prefetch()],
   build: {
     inlineStylesheets: 'auto',
